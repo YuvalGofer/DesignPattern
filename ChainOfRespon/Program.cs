eb15c29ac2e31133dcfe9cf6150eb5996f14d6cf
yuval gofer <yuvyael@gmail.com>
2022-04-13 21:13:22 +0300

    Add project files.

---
+++ ChainOfRespon/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static void RandNumber(int rng)
+{
+    Random random = new Random();
+    int _rng = random.Next(1, 10);
+    bool isFix = false;
+    //string _rng = new Random(1, 10);
+    if (_rng > 5)
+    {
+        isFix = true;
+        Console.WriteLine($"Is fix: {isFix}");
+    }
+    else
+    {
+        Console.WriteLine($"Is fix: {isFix}");
+    }
+
+}
---
+++ FactoryMethods/Diets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethods
+{
+    class DietFactory
+    {
+        public Diets GetDiets(string TypeOfDiet,string id )
+        {
+            if (id == "v") return GetDiets(TypeOfDiet, 1);
+            if (id == "o") return GetDiets(TypeOfDiet, 2);
+            if (id == "m") return GetDiets(TypeOfDiet, 3);
+        }
+        public Diets GetDiets(string TypeOfDiet, in id)
+        {
+
+        }
+    }
+    abstract class Diets
+    {
+        public string TypeOfDiet { get; set; }
+
+        public bool IsDiet { get; set; }
+        public string Error { get; set; }
+    }
+}
